102722ebe5aefeef9dad501618a4b5b0c375a0da
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2015-01-29 15:21:58 +0000

    Consertando o problema de conversão de entidades não inicializadas pelo Jackson

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/bean/message/HibernateAwareObjectMapper.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/bean/message/HibernateAwareObjectMapper.java
@@ -5,18 +5,34 @@
  */
 package br.com.cds.connecta.framework.core.bean.message;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import java.io.File;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
 
 
 /**
  *
  * @author Vinicius
  */
+@Component
 public class HibernateAwareObjectMapper extends ObjectMapper {
 
     public HibernateAwareObjectMapper() {
-        registerModule(new Hibernate4Module());
+        final Hibernate4Module hibernate4Module = new Hibernate4Module();
+        
+//        hibernate4Module.enable(Hibernate4Module.Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS);
+        
+        registerModule(hibernate4Module);
+    }
+
+    @Override
+    public void writeValue(File resultFile, Object value) throws IOException, JsonGenerationException, JsonMappingException {
+        System.out.println("AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        super.writeValue(resultFile, value); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
