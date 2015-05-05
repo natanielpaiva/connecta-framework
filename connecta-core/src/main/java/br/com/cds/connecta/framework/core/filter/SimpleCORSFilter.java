387fca3487d70bc92eda6f285694c82c7e5e6b18
raphael.soares <raphael.soares@7041b7d6-f801-4286-9b3d-420f2f459141>
2015-05-05 17:34:31 +0000

    notify

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/filter/SimpleCORSFilter.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/filter/SimpleCORSFilter.java
@@ -18,18 +18,20 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-		chain.doFilter(req, res);
-	}
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
 
     @Override
-	public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
     @Override
-	public void destroy() {}
-    
+    public void destroy() {
+    }
+
 }
