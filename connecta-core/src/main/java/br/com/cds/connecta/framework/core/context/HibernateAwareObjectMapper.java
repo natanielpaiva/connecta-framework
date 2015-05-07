97ae2037eec65f851ac8104a09bd36f16376818d
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-05-07 19:24:36 +0000

    Ajuste no modo de inclusão de propriedades na serialização do JSON

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/context/HibernateAwareObjectMapper.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/context/HibernateAwareObjectMapper.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.cds.connecta.framework.core.context;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
@@ -22,9 +18,10 @@ public class HibernateAwareObjectMapper extends ObjectMapper {
         Hibernate4Module hibernate4Module = new Hibernate4Module();
         
         hibernate4Module.configure(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION, false);
-        
+
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        
+       
+        setSerializationInclusion(JsonInclude.Include.NON_NULL);
         registerModule(hibernate4Module);
     }
     
