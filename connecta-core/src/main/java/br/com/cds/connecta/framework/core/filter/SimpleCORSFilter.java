21931bdcdda34fd76610aa8c7043e50e58533396
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-05-13 17:56:00 +0000

    Ajustando o SimpleCors, pois não estava permitindo alguns  headers

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/filter/SimpleCORSFilter.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/filter/SimpleCORSFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -18,11 +19,13 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //Origin, X-Requested-With, Content-Type, Accept
+        response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
         chain.doFilter(req, res);
     }
 
