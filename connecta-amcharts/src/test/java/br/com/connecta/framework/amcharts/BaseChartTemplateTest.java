8411098ccd684591c97aa4751afad76e9353d8af
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2015-05-18 20:58:53 +0000

    Reaproveitando mais codigo

---
+++ connecta-amcharts/src/test/java/br/com/connecta/framework/amcharts/BaseChartTemplateTest.java
@@ -0,0 +1,32 @@
+package br.com.connecta.framework.amcharts;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.apache.commons.io.IOUtils;
+
+@JsonSubFolder
+public class BaseChartTemplateTest {
+    private static final String FILE_TEMPLATE = "src/test/resources/chart-templates/%s/%s.json";
+    
+    protected ObjectMapper mapper = new ObjectMapper();
+    
+    protected String json(String json) throws IOException {
+        final JsonSubFolder annotation = this.getClass().getAnnotation(JsonSubFolder.class);
+        return IOUtils.toString(new FileInputStream(
+                        String.format(FILE_TEMPLATE,annotation.value(),json)
+                )
+            );
+    }
+    
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface JsonSubFolder {
+    String value() default "";
+}
