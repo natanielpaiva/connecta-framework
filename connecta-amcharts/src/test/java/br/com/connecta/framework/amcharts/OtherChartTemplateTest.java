04c2f8f2fca061bbb8f9a887e60d0db84350a2a4
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-05-22 17:18:45 +0000

    Criando os testes para os Other charts

--- connecta-amcharts/src/test/java/br/com/connecta/framework/amcharts/OtherChartTemplateTest.java
+++ connecta-amcharts/src/test/java/br/com/connecta/framework/amcharts/OtherChartTemplateTest.java
@@ -2,12 +2,14 @@ package br.com.connecta.framework.amcharts;
 
 import br.com.cds.connecta.framework.amcharts.model.AmAngularGauge;
 import br.com.cds.connecta.framework.amcharts.model.AmFunnelChart;
+import br.com.cds.connecta.framework.amcharts.model.AmSerialChart;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import org.junit.Ignore;
 import org.junit.Test;
 
 @JsonSubFolder("other")
@@ -16,7 +18,7 @@ public class OtherChartTemplateTest extends BaseChartTemplateTest {
     @Test
     public void otherAngularGauge() throws IOException {
         AmAngularGauge chart = mapper.readValue(json("other-angular-gauge"), AmAngularGauge.class);
-        
+
         assertThat(chart.getType(), is("gauge"));
         assertThat(chart.getPath(), is("https://www.amcharts.com/lib/3/"));
         assertThat(chart.getArrows().get(0).getId(), is("GaugeArrow-1"));
@@ -25,33 +27,83 @@ public class OtherChartTemplateTest extends BaseChartTemplateTest {
         assertThat(chart.getAxes().get(0).getEndValue(), is(220d));
         assertThat(chart.getAxes().get(0).getId(), is("GaugeAxis-1"));
         assertThat(chart.getAxes().get(0).getValueInterval(), is(10d));
-        
+
         testBands(chart, 0, "#00CC00", 90d, "GaugeBand-1", 0d);
         testBands(chart, 1, "#ffac29", 130d, "GaugeBand-2", 90d);
         testBands(chart, 2, "#ea3838", 220d, "GaugeBand-3", 130d);
         assertThat(chart.getAxes().get(0).getBands().get(2).getInnerRadius(), is("95%"));
-        
+
         assertThat(chart.getTitles().get(0).getId(), is("Title-1"));
         assertThat(chart.getTitles().get(0).getSize(), is(15d));
         assertThat(chart.getTitles().get(0).getText(), is("Speedometer"));
 
     }
-    
+
     @Test
     public void otherFunnel() throws IOException {
         AmFunnelChart chart = mapper.readValue(json("other-funnel"), AmFunnelChart.class);
+
+        testeGeneral(chart);
+
+        assertThat(chart.getNeckHeight(), is("30%"));
+        assertThat(chart.getNeckWidth(), is("40%"));
+
+        testDataProvidersFunnel(chart);
+
+    }
+
+    @Test
+    public void otherPyramid() throws IOException {
+        AmFunnelChart chart = mapper.readValue(json("other-pyramid"), AmFunnelChart.class);
+        testeGeneral(chart);
+        assertThat(chart.getRotate(), is(true));
+
+        testDataProvidersFunnel(chart);
+
+    }
+    
+    @Test
+    public void otherCandlestick() throws IOException{
+        AmSerialChart chart = mapper.readValue(json("other-candlestick"), AmSerialChart.class);
         
+        assertThat(chart.getType(), is("serial"));
+        assertThat(chart.getPath(), is("https://www.amcharts.com/lib/3/"));
+        assertThat(chart.getCategoryField(), is("date"));
+        assertThat(chart.getCategoryAxis().getParseDates(), is(true));
+        assertThat(chart.getChartScrollbar().getGraph().toString(), is("g1"));
+        assertThat(chart.getChartScrollbar().getGraphType(), is("line"));
+        assertThat(chart.getChartScrollbar().getScrollbarHeight(), is(30d));
+    }
+
+    private void testeGeneral(AmFunnelChart chart) {
         assertThat(chart.getType(), is("funnel"));
         assertThat(chart.getPath(), is("https://www.amcharts.com/lib/3/"));
         assertThat(chart.getBalloonText(), is("[[title]]:<b>[[value]]</b>"));
         assertThat(chart.getLabelPosition(), is("right"));
-        assertThat(chart.getNeckHeight(), is("30%"));
-        assertThat(chart.getNeckWidth(), is("40%"));
         assertThat(chart.getMarginLeft(), is(15d));
         assertThat(chart.getMarginRight(), is(160d));
         assertThat(chart.getTitleField(), is("title"));
         assertThat(chart.getValueField(), is("value"));
-        
+    }
+
+    private void testBands(AmAngularGauge chart, int i, String color, Double endValue,
+            String id, Double startValue) {
+        assertThat(chart.getAxes().get(0).getBands().get(i).getColor().toString(), is(color));
+        assertThat(chart.getAxes().get(0).getBands().get(i).getEndValue(), is(endValue));
+        assertThat(chart.getAxes().get(0).getBands().get(i).getId(), is(id));
+        assertThat(chart.getAxes().get(0).getBands().get(i).getStartValue(), is(startValue));
+    }
+
+    private void testDataProviders(List<Object> dataProvider, String[] categories, int[] columns) {
+        for (int i = 0; i < dataProvider.size(); i++) {
+            Map provider = (Map<String, Object>) dataProvider.get(i);
+            assertThat((String) provider.get("title"), equalTo(categories[i]));
+            assertThat((Integer) provider.get("value"), equalTo(columns[i]));
+        }
+    }
+
+    private void testDataProvidersFunnel(AmFunnelChart chart) {
+
         String[] categories;
         categories = new String[]{
             "Website visits",
@@ -62,6 +114,7 @@ public class OtherChartTemplateTest extends BaseChartTemplateTest {
             "Asked for support",
             "Purchased more"
         };
+        
         int[] columns;
         columns = new int[]{
             300,
@@ -72,25 +125,8 @@ public class OtherChartTemplateTest extends BaseChartTemplateTest {
             25,
             18
         };
-        
+
         testDataProviders(chart.getDataProvider(), categories, columns);
-
-    }
-
-    private void testBands(AmAngularGauge chart, int i, String color, Double endValue,
-                            String id, Double startValue) {
-        assertThat(chart.getAxes().get(0).getBands().get(i).getColor().toString(), is(color));
-        assertThat(chart.getAxes().get(0).getBands().get(i).getEndValue(), is(endValue));
-        assertThat(chart.getAxes().get(0).getBands().get(i).getId(), is(id));
-        assertThat(chart.getAxes().get(0).getBands().get(i).getStartValue(), is(startValue));
-    }
-    
-     private void testDataProviders(List<Object> dataProvider, String[] categories, int[] columns) {
-        for (int i = 0; i < dataProvider.size(); i++) {
-            Map provider = (Map<String, Object>) dataProvider.get(i);
-            assertThat((String) provider.get("title"), equalTo(categories[i]));
-            assertThat((Integer) provider.get("value"), equalTo(columns[i]));
-        }
     }
 
 }
