f28e026b08c76c3cb57507d12688893a020c692e
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-06-03 16:02:15 +0000

    Retirado texto de template do Netbeans

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/test/MockMvcProvider.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/test/MockMvcProvider.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.cds.connecta.framework.core.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +12,7 @@ import org.springframework.web.context.WebApplicationContext;
  */
 @Component
 public class MockMvcProvider {
+    
     @Autowired
     private WebApplicationContext wac;
     
