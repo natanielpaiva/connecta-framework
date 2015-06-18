6fc4c94126f581fae64e163779af18cf61cfd7f0
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-06-18 19:00:58 +0000

    Voltando o nome do atributo para count, pois este ja esta sendo utilizado com este nome em TODOS os javascripts...

--- connecta-core/src/test/java/br/com/cds/connecta/framework/core/filter/PaginationFilterTest.java
+++ connecta-core/src/test/java/br/com/cds/connecta/framework/core/filter/PaginationFilterTest.java
@@ -34,7 +34,7 @@ public class PaginationFilterTest {
             filter.setPage(page+1);
         }
         if (size != null) {
-            filter.setSize(size);
+            filter.setCount(size);
         }
         if (sort != null) {
             filter.setSort(sort);
