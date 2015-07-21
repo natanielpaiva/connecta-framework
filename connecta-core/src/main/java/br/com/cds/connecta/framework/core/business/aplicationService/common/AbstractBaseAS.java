ae01af1e783a2415aa6ef4e12280708e63cafdd0
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-07-21 14:22:44 +0000

    Ajuste

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/business/aplicationService/common/AbstractBaseAS.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/business/aplicationService/common/AbstractBaseAS.java
@@ -5,18 +5,43 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import br.com.cds.connecta.framework.core.entity.AbstractBaseEntity;
+import br.com.cds.connecta.framework.core.filter.PaginationFilter;
+import org.springframework.data.domain.Page;
 
 @Service
 public abstract class AbstractBaseAS<E extends AbstractBaseEntity> {
 
-    public abstract E get(Long id) throws Exception;
+    public E save(E entity) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public E saveOrUpdate(E entity) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    };
 
-    public abstract List<E> list() throws Exception;
+    public E update(E entity) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-    public abstract E saveOrUpdate(E entity) throws Exception;
+    public E get(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-    public abstract void delete(Long id) throws Exception;
+    public List<E> list() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-    public abstract void delete(E entity) throws Exception;
+    public Page<E> find(PaginationFilter filter) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void delete(E entity) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void delete(Long id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
 
 }
