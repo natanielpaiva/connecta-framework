179be46e9b3312030249ee574925a818054ab75b
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-08-06 14:42:37 +0000

    Ajustando o Color para o Jackson conseguir mostrar

--- connecta-amcharts/src/main/java/br/com/cds/connecta/framework/amcharts/model/addition/Color.java
+++ connecta-amcharts/src/main/java/br/com/cds/connecta/framework/amcharts/model/addition/Color.java
@@ -1,19 +1,24 @@
 package br.com.cds.connecta.framework.amcharts.model.addition;
 
+import br.com.cds.connecta.framework.amcharts.model.serializer.ColorSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.io.Serializable;
 
-public class Color implements Serializable{
+@JsonSerialize(using = ColorSerializer.class)
+public class Color implements Serializable {
 
     public static final Color BLACK = create("000000");
     public static final Color WHITE = create("FFFFFF");
 
     private String colorHexVal;
 
-    private Color() {};
+    private Color() {
+    }
 
     private Color(String colorHexVal) {
         this.colorHexVal = colorHexVal;
     }
+
     public static Color create(String colorHexVal) {
         return new Color(colorHexVal);
     }
@@ -21,6 +26,6 @@ public class Color implements Serializable{
     // Append # to color values as we have hex values
     @Override
     public String toString() {
-        return  this.colorHexVal;
+        return this.colorHexVal;
     }
 }
