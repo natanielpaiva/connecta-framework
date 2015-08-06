4e2ba6b0e50eb8ea64daacb9a9bb66294b7cffc1
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-08-06 14:32:27 +0000

    Ajustes Security

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/security/SecurityConnector.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/security/SecurityConnector.java
@@ -4,9 +4,7 @@ import br.com.cds.connecta.framework.core.domain.security.AuthenticationDTO;
 import br.com.cds.connecta.framework.core.http.RestClient;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 
 /**
@@ -14,28 +12,34 @@ import org.springframework.web.client.RestClientException;
  * @author Julio Lemes
  * @date Jul 27, 2015
  */
-@Component
 public class SecurityConnector {
-    
-    @Value("${auth.provider.url}")
-    private String AUTH_PROVIDER_URL;
-    
-    private String AUTH_ENDPOINT;
-    private String GET_AUTH_USER_ENDPOINT;
-    
+
+    private String authProviderUrl;
+    private String authEndpoint;
+    private String getAuthUserEndpoint;
+
     @PostConstruct
-    public void initializeURIs(){
-        AUTH_ENDPOINT = AUTH_PROVIDER_URL + "/auth";
-        GET_AUTH_USER_ENDPOINT = AUTH_ENDPOINT + "/{token}";
+    public void initializeURIs() {
+        authEndpoint = authProviderUrl + "/auth";
+        getAuthUserEndpoint = authEndpoint + "/{token}";
     }
 
     public AuthenticationDTO verifyAuthentication(String token) {
         try {
-            return RestClient.request(GET_AUTH_USER_ENDPOINT, HttpMethod.GET, AuthenticationDTO.class, null, null, token);
+            return RestClient.request(getAuthUserEndpoint, HttpMethod.GET, AuthenticationDTO.class, null, null, token);
         } catch (RestClientException e) {
             Logger.getLogger(SecurityConnector.class.getSimpleName()).severe(e.getMessage());
         }
-        
+
         return null;
     }
+
+    public String getAuthProviderUrl() {
+        return authProviderUrl;
+    }
+
+    public void setAuthProviderUrl(String authProviderUrl) {
+        this.authProviderUrl = authProviderUrl;
+    }
+
 }
--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/security/SecurityInterceptor.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/security/SecurityInterceptor.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
@@ -15,30 +14,30 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * @date Jul 27, 2015
  */
 public class SecurityInterceptor extends HandlerInterceptorAdapter {
-    
-    @Autowired SecurityConnector connector;
-    
+
+    private SecurityConnector connector;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         final HttpServletRequest req = (HttpServletRequest) request;
         final HttpServletResponse resp = (HttpServletResponse) response;
-        
+
         String userToken = req.getHeader("Authorization");
         Boolean hasToken = userToken != null && !userToken.isEmpty();
 
         if (!hasToken) {
             for (Cookie cookie : request.getCookies()) {
-                if(cookie.getName().equals("Authorization")){
+                if (cookie.getName().equals("Authorization")) {
                     userToken = cookie.getValue();
                     hasToken = true;
                     break;
                 }
             }
         }
-        
-        if(hasToken){
+
+        if (hasToken) {
             AuthenticationDTO auth = connector.verifyAuthentication(userToken);
-            if(Util.isNotNull(auth)){
+            if (Util.isNotNull(auth)) {
                 return true;
             }
         }
@@ -46,9 +45,17 @@ public class SecurityInterceptor extends HandlerInterceptorAdapter {
         sendUnauthorized(req, resp);
         return false;
     }
-    
+
     protected void sendUnauthorized(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendError(401);
     }
 
+    public SecurityConnector getConnector() {
+        return connector;
+    }
+
+    public void setConnector(SecurityConnector connector) {
+        this.connector = connector;
+    }
+    
 }
