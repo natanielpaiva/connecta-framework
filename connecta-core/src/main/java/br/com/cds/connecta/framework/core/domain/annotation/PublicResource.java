12291ce68c56d409d23f671a3464584917e3f880
Julio Lemes <juliolemesti@users.noreply.github.com>
2015-08-11 19:30:28 +0000

    Ajustes Security Adicionada annotation @PublicResource para metodos e classes que deverão ter acesso público

---
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/domain/annotation/PublicResource.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cds.connecta.framework.core.domain.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Julio Lemes
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface PublicResource {}
