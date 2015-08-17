31d528a1b395b5119d9e0a06c9ecf0cfe14d6fb7
raphael.soares <raphael.soares@7041b7d6-f801-4286-9b3d-420f2f459141>
2015-08-17 19:04:20 +0000

    Correções de bugs, speaknow

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/persistence/jpa/common/AbstractBaseJpaDAO.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/persistence/jpa/common/AbstractBaseJpaDAO.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.cds.connecta.framework.core.entity.AbstractBaseEntity;
 import br.com.cds.connecta.framework.core.filter.PaginationFilter;
 import br.com.cds.connecta.framework.core.util.Util;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -40,7 +41,7 @@ public abstract class AbstractBaseJpaDAO<E extends AbstractBaseEntity> {
     protected EntityManager getEntityManager() {
         return entityManager;
     }
-    
+
     @SuppressWarnings("unchecked")
     public E get(Long id) {
         return (E) getEntityManager().find(getEntityClass(), id);
@@ -92,7 +93,7 @@ public abstract class AbstractBaseJpaDAO<E extends AbstractBaseEntity> {
         if (Util.isNull(entity)) {
             throw new IllegalArgumentException("A entidade não pode ser nula.");
         }
-        
+
         getEntityManager().remove(entity);
         if (flush) {
             flush();
@@ -145,19 +146,27 @@ public abstract class AbstractBaseJpaDAO<E extends AbstractBaseEntity> {
     protected Query createNativeQuery(String namedQuery, Class<?> resultClass) {
         return getEntityManager().createNativeQuery(namedQuery, resultClass);
     }
-    
+
     protected PageImpl<E> search(PaginationFilter filter, String searchNQ, String countNQ) {
         //Retorna o total de resultados
         Query count = createNamedQuery(countNQ);
         for (String key : filter.getFilter().keySet()) {
-            count.setParameter(key, "%" + filter.getFilter().get(key) + "%");
+            if (isParameterDefault(key)) {
+                count.setParameter(key, filter.getFilter().get(key));
+            } else {
+                count.setParameter(key, "%" + filter.getFilter().get(key) + "%");
+            }
         }
         setParametersWithoutValues(count, filter.getFilter());
-        Long total = (Long)count.getSingleResult();
+        Long total = (Long) count.getSingleResult();
         //Retorna a lista de resultados
         Query query = createNamedQuery(searchNQ);
         for (String key : filter.getFilter().keySet()) {
-            query.setParameter(key, "%" + filter.getFilter().get(key) + "%");
+            if (isParameterDefault(key)) {
+                query.setParameter(key, filter.getFilter().get(key));
+            } else {
+                query.setParameter(key, "%" + filter.getFilter().get(key) + "%");
+            }
         }
         setParametersWithoutValues(query, filter.getFilter());
         query.setFirstResult((filter.getPage() - 1) * 10);
@@ -166,22 +175,27 @@ public abstract class AbstractBaseJpaDAO<E extends AbstractBaseEntity> {
         //Cria o Page
         return new PageImpl<>(resultList, filter.makePageable(), total);
     }
-    
-      
+
     /**
      * Método que seta as variáveis não setadas pelo filter como nulas
+     *
      * @param query
-     * @param filter 
+     * @param filter
      */
-    private void setParametersWithoutValues(Query query, HashMap<String, String> filter) {
+    private void setParametersWithoutValues(Query query, HashMap<String, Object> filter) {
         Set<String> withValues = filter.keySet();
         Set<Parameter<?>> parameters = query.getParameters();
         Iterator<Parameter<?>> iterator = parameters.iterator();
         while (iterator.hasNext()) {
             Parameter<?> next = iterator.next();
-            if(!withValues.contains(next.getName())){
+            if (!withValues.contains(next.getName())) {
                 query.setParameter(next.getName(), null);
             }
         }
+    }
+    
+    public boolean isParameterDefault(String parameter){
+        String[] parameters = {"userId", "companies"};
+        return Arrays.asList(parameters).contains(parameter);
     }
 }
