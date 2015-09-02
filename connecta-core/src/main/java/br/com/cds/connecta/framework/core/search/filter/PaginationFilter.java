7ff68a34a8ef3e856f157b04075ef87230758680
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2015-09-02 20:42:30 +0000

    Adicionando suporte para pesquisa full-text

--- connecta-core/src/main/java/br/com/cds/connecta/framework/core/search/filter/PaginationFilter.java
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/search/filter/PaginationFilter.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.cds.connecta.framework.core.filter;
+package br.com.cds.connecta.framework.core.search.filter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
