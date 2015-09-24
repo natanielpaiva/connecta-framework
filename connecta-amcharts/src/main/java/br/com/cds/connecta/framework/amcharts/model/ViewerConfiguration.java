3c8e4735980d56afd026c3cfcb87e562deb6127e
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2015-09-24 16:57:53 +0000

    Refactoring de Entidades de Viewer, retirando coisas especificas do Presenter do Framework.

---
+++ connecta-amcharts/src/main/java/br/com/cds/connecta/framework/amcharts/model/ViewerConfiguration.java
@@ -0,0 +1,11 @@
+package br.com.cds.connecta.framework.amcharts.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Vinicius Pires <vinicius.pires@cds.com.br>
+ */
+public interface ViewerConfiguration extends Serializable {
+
+}
--- connecta-amcharts/src/main/java/br/com/cds/connecta/framework/amcharts/model/helper/Status.java
+++
@@ -1,6 +0,0 @@
-package br.com.cds.connecta.framework.amcharts.model.helper;
-
-public enum Status {
-    DO_NOT_OVERWRITE, SUCCESS, ERROR
-
-}
