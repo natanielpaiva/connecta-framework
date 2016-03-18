e2c2ebb9026eca18c62079c304ef6ef89b5e7982
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2016-03-18 18:01:11 +0000

    Adicionando a annotation @ConnectaViewer para passar o nome do módulo para o documento a ser indexado no Solr.

---
+++ connecta-core/src/main/java/br/com/cds/connecta/framework/core/search/annotation/ConnectaViewer.java
@@ -0,0 +1,16 @@
+package br.com.cds.connecta.framework.core.search.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Vinicius Pires <vinicius.pires@cds.com.br>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConnectaViewer {
+    String module();
+}
